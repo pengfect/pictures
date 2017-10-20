584cf983fc0f2e390c02ef4946a0af3eec6da80f
彭飞 <541015064@qq.com>
2017-10-20 09:15:05 +0800

    add the person profile page and picture detail page

--- pictures/logic.py
+++ pictures/logic.py
@@ -3,3 +3,12 @@ from pictures.models import User,Image,Comment
 def show_pic(page=1,per_page=5):
     images = Image.query.paginate(page, per_page).items
     return images
+
+
+def user_by_id(userId):
+    user = User.query.filter_by(id=userId).first()
+    return user
+
+def image_by_id(imageId):
+    image = Image.query.filter_by(id=imageId).first()
+    return image
--- pictures/views.py
+++ pictures/views.py
@@ -1,5 +1,17 @@
-from pictures import app
+from pictures import app,logic
+from flask import render_template
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    images = logic.show_pic()
+    return render_template('index.html',images=images)
+
+@app.route('/profile/<int:userId>/')
+def profile(userId):
+    user = logic.user_by_id(userId)
+    return render_template('profile.html',user=user)
+
+@app.route('/detail/<int:imageId>/')
+def detail(imageId):
+    image = logic.image_by_id(imageId)
+    return render_template('detail.html',image=image)
