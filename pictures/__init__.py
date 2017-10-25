0142cd38f5eff8555696e193d36b57c1d5659b7e
彭飞 <541015064@qq.com>
2017-10-25 10:36:38 +0800

    add the navigation bar (base.html)

--- pictures/__init__.py
+++ pictures/__init__.py
@@ -1,4 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+
 
 app = Flask(__name__)
+app.config.from_pyfile('./app.conf')
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+Bootstrap(app)
+moment = Moment(app)
 from pictures import views
